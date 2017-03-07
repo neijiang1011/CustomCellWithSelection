72254fb51a5611466653fb09672210559c6fdca9
USER <user@mymacpro.local>
2017-03-07 15:34:23 +0800

    Initial Commit

---
+++ CustomCellWithSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCellWithSelection
+//
+//  Created by mobile manager on 17/3/7.
+//  Copyright © 2017年 YTPersonal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
